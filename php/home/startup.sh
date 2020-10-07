f5bd137efb928a6950ad546a45b21215ffd1ab80
Charley Wu <akunzai@gmail.com>
2020-10-07 11:16:11 +0800

    Update startup.sh
    - fix: Cannot load Zend OPcache - it was already loaded
    - fix: Permission denied error caused by empty `which php` in cron jobs
    - write cron output and error to docker logs

--- php/home/startup.sh
+++ php/home/startup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
+# preferred mirror site
 grep free.nchc.org.tw /etc/apt/sources.list >/dev/null || sed -i 's,^\deb http://deb.debian.org,deb https://free.nchc.org.tw,g' /etc/apt/sources.list
+# fix: Cannot load Zend OPcache - it was already loaded
+sed -i '/zend_extension=opcache/d' /usr/local/etc/php/conf.d/php.ini
+# fix: Permission denied error caused by empty `which php` in cron jobs
+[ -e /usr/bin/php ] || ln -s /usr/local/bin/php /usr/bin/php
 
 (hash cron && hash gosu ) >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
@@ -17,5 +21,5 @@ fi
 # /proc/1/fd/1: STDOUT for Docker
 # /proc/1/fd/2: STDERR for Docker
 (
-echo "* * * * * [[ -x /home/site/wwwroot/cron.sh ]] && /usr/sbin/gosu www-data /home/site/wwwroot/cron.sh >/home/logs/cron.log 2>&1";
+echo "* * * * * [ -x /home/site/wwwroot/cron.sh ] && (/usr/sbin/gosu www-data /home/site/wwwroot/cron.sh)>/proc/1/fd/1 2>/proc/1/fd/2"
 ) | crontab
