9f46c6a325a98effd167f1358ab3b379783f8a97
Charley Wu <akunzai@gmail.com>
2022-01-04 11:37:06 +0800

    no needs to remove duplicated xdebug configuration in native PHP image
    - only needs in Azure App Service PHP image

--- php/home/startup.sh
+++ php/home/startup.sh
@@ -22,13 +22,6 @@ if [ -e /usr/local/etc/php/conf.d/php.ini ]; then
     sed -i '/zend_extension=opcache/d' /usr/local/etc/php/conf.d/php.ini
 fi
 
-if [ -e /usr/local/etc/php/conf.d/xdebug.ini ]; then
-    # fix: Cannot load Xdebug - it was already loaded
-    if grep -q zend_extension "/usr/local/etc/php/conf.d/xdebug.ini"; then
-        sed -i '/zend_extension/d' /usr/local/etc/php/conf.d/xdebug.ini
-    fi
-fi
-
 # fix: Require ip not working
 if ! grep -q RemoteIPHeader /etc/apache2/apache2.conf; then
     echo -e "RemoteIPHeader X-Forwarded-For\nRemoteIPInternalProxy 172.16.0.0/12" >> /etc/apache2/apache2.conf
