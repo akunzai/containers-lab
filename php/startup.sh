c65fe7c13cdf7f52e8a2d33a39747e54ade5091c
Charley Wu <akunzai@gmail.com>
2022-05-05 12:51:49 +0800

    Simplify the startup.sh

--- php/startup.sh
+++ php/startup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# setup host alias
-hash host >/dev/null 2>&1
+# fix: Require ip not working
+if ! grep -q RemoteIPHeader /etc/apache2/apache2.conf; then
+    echo -e "RemoteIPHeader X-Forwarded-For\nRemoteIPInternalProxy 172.16.0.0/12" >> /etc/apache2/apache2.conf
+fi
+
+# install tools
+hash gosu >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
     apt-get update -qq >/dev/null 2>&1
-    apt-get install --no-install-recommends -yqq dnsutils iproute2
+    apt-get install --no-install-recommends -yqq cron gosu dnsutils iproute2
 fi
+
+# setup host alias
 host host.docker.internal >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
     ip route show | awk '/default/ {print $3,"host.docker.internal"}' >> /etc/hosts
@@ -13,28 +20,15 @@ fi
 
 if [ -d "$APACHE_DOCUMENT_ROOT" -a "$APACHE_DOCUMENT_ROOT" != "/var/www/html" ]; then
     # link /var/www/html to APACHE_DOCUMENT_ROOT
-    rm -rf /var/www/html;
-    ln -s $APACHE_DOCUMENT_ROOT /var/www/html;
-fi
-
-if [ -e /usr/local/etc/php/conf.d/php.ini ]; then
-    # fix: Cannot load Zend OPcache - it was already loaded
-    sed -i '/zend_extension=opcache/d' /usr/local/etc/php/conf.d/php.ini
-fi
-
-# fix: Require ip not working
-if ! grep -q RemoteIPHeader /etc/apache2/apache2.conf; then
-    echo -e "RemoteIPHeader X-Forwarded-For\nRemoteIPInternalProxy 172.16.0.0/12" >> /etc/apache2/apache2.conf
+    rm -rf /var/www/html
+    ln -s $APACHE_DOCUMENT_ROOT /var/www/html
+    # make sure permission
+    chown -R www-data:www-data $APACHE_DOCUMENT_ROOT &
 fi
 
 if [ -e "$APACHE_DOCUMENT_ROOT/cron.sh" ]; then
     # fix: Permission denied error caused by empty `which php` in cron jobs
     [ -e /usr/bin/php ] || ln -s /usr/local/bin/php /usr/bin/php
-    (hash cron && hash gosu ) >/dev/null 2>&1
-    if [ "$?" -ne "0" ]; then
-        apt-get update -qq >/dev/null 2>&1
-        apt-get install --no-install-recommends -yqq cron gosu
-    fi
     service cron stauts >/dev/null 2>&1
     if [ "$?" -ne "0" ]; then
         service cron start
