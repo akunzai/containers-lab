845c9ce2e3a206c62185ea582cde65e35f5a6e23
Charley Wu <akunzai@gmail.com>
2024-08-24 21:38:18 +0800

    Simplify the NRPE container

---
+++ nrpe/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+set -e
+
+if [ "$1" == '/usr/sbin/nrpe' ]; then
+
+	[ -e /var/run/nagios/nrpe.pid ] && rm /var/run/nagios/nrpe.pid
+
+	$@
+
+	# keep the container running
+	tail -f /var/log/nrpe.log
+fi
+
+exec "$@"
